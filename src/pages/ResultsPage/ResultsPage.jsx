49948ebe785b7d30bdd9da96b0daa6ed3de2bdc5
saifk <ec22616@qmul.ac.uk>
2023-05-23 18:39:42 +0100

    second commit

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -11,18 +11,19 @@ function ResultsPage() {
     const [results, setResults] = useState([])
 
     const {id} = useParams();
+    // console.log(process.env)
 
 
     useEffect(() => {
         switch(location.pathname){
             case(`/search/${id}`):
-            Axios.get(`https://www.googleapis.com/customsearch/v1?key=AIzaSyASZ-8EAOI-WIoEfOejUTQdASG2yuDyoxE&cx=74fe0f52a88044d9e&q=${id}`).then((response) => {
+            Axios.get(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_KEY}&cx=74fe0f52a88044d9e&q=${id}`).then((response) => {
                 console.log(response)
                 setResults(response.data.items)
             })
 
             case(`/search/${id}/images`):
-            Axios.get(`https://www.googleapis.com/customsearch/v1?key=AIzaSyASZ-8EAOI-WIoEfOejUTQdASG2yuDyoxE&cx=74fe0f52a88044d9e&searchType=image&q=${id}`).then((response) => {
+            Axios.get(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_KEY}&cx=74fe0f52a88044d9e&searchType=image&q=${id}`).then((response) => {
                 console.log(response)
                 setResults(response.data.items)
             })
